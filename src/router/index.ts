430315d4fa0db940af25243dbd653e62d41a0a49
benaor <benjamingirard25@gmail.com>
2022-03-17 00:03:36 +0100

    initialize project with typescript, scss, vuex, vue-router ...

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
+import Home from "../views/Home.vue"
+import Basket from "../views/Basket.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "Home",
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: "/basket",
+    name: "Basket",
+    component: Basket
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
